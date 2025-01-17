1b07b3b746cafc1d3fce09ea2cb40282c935d839
Nyoman Sunima <nyomansunima@gmail.com>
2025-01-17 09:40:05 +0800

    🚕 feat(ui): create subscribe form

---
+++ src/features/updates/subscribe-form.tsx
@@ -0,0 +1,59 @@
+'use client'
+
+import * as React from 'react'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  Button,
+  Label,
+  Input,
+} from '@shared/components'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+const formSchema = z.object({
+  email: z
+    .string()
+    .email('Opps, your email is look bad')
+    .min(2, 'Must be at least 2 characters'),
+})
+
+export function SubscribeForm(): React.ReactElement {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {}
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col gap-2 w-full"
+      >
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input placeholder="Your email address" {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Subscribe now</Button>
+      </form>
+    </Form>
+  )
+}
