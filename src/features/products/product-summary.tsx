59295cbe0cf325a366eaeeed9a24a47ab2d41b39
Nyoman Sunima <nyomansunima@gmail.com>
2025-01-17 09:41:23 +0800

    🚕 feat(ui): add product summary

---
+++ src/features/products/product-summary.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react'
+
+export function ProductSummary(): React.ReactElement {
+  return (
+    <div className="flex flex-col border border-border p-3 rounded-2xl gap-3">
+      {/* version */}
+      <div className="flex gap-1">
+        <h3 className="flex items-center gap-1 flex-1">
+          <i className="fi fi-rr-circle-small" />
+          Version
+        </h3>
+        <span className="text-foreground/60">v1.0.0</span>
+      </div>
+
+      {/* platfroms */}
+      <div className="flex gap-1">
+        <h3 className="flex items-center gap-1 flex-1">
+          <i className="fi fi-rr-circle-small" />
+          Platform suported
+        </h3>
+        <span className="text-foreground/60">Framer, Webflow, NextJs</span>
+      </div>
+
+      {/* License */}
+      <div className="flex gap-1">
+        <h3 className="flex items-center gap-1 flex-1">
+          <i className="fi fi-rr-circle-small" />
+          License
+        </h3>
+        <span className="text-foreground/60">Commercial</span>
+      </div>
+    </div>
+  )
+}
