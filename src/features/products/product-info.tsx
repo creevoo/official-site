16979a3bcdfb3e0cff6ff45ee419dec257a0c3b6
Nyoman Sunima <nyomansunima@gmail.com>
2025-01-17 09:42:53 +0800

    🚕 feat(ui): add product info

---
+++ src/features/products/product-info.tsx
@@ -0,0 +1,16 @@
+import { Button } from '@shared/components'
+import * as React from 'react'
+import { ProductLinkDialog } from './product-link-dialog'
+
+export function ProductInfo(): React.ReactElement {
+  return (
+    <div className="flex gap-2">
+      <div className="flex flex-col flex-1">
+        <h1 className="text-xl">Breain - Simple developer portfolio</h1>
+        <span className="text-foreground/60 mt-1">$25.00</span>
+      </div>
+
+      <ProductLinkDialog />
+    </div>
+  )
+}
