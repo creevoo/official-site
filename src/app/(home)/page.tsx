c055f59350e1d592574db274e7700f63de9fd224
Nyoman Sunima <nyomansunima@gmail.com>
2025-01-17 09:45:50 +0800

    🚕 feat: add home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,25 +3,23 @@ import { Metadata } from 'next'
 import { sharedMetadata } from '@shared/libs'
 
 export const metadata: Metadata = {
-  title: 'YOUR_TITLE_META_TAG',
-  description: 'YOUR_DESCRIPTION_META_TAG',
+  title: 'Stunning Website Templates - Creevoo',
+  description:
+    'Get free and use stunning website template for your landing page, blog, portfolio and business',
   openGraph: {
     ...sharedMetadata.openGraph,
-    title: 'YOUR_TITLE_META_TAG',
-    description: 'YOUR_DESCRIPTION_META_TAG',
+    title: 'Stunning Website Templates - Creevoo',
+    description:
+      'Get free and use stunning website template for your landing page, blog, portfolio and business',
   },
   twitter: {
     ...sharedMetadata.twitter,
-    title: 'YOUR_TITLE_META_TAG',
-    description: 'YOUR_DESCRIPTION_META_TAG',
+    title: 'Stunning Website Templates - Creevoo',
+    description:
+      'Get free and use stunning website template for your landing page, blog, portfolio and business',
   },
 }
 
 export default function HomePage(): React.ReactElement {
-  return (
-    <div className="flex flex-col gap-10">
-      {/* TODO: Importa all of your page section and detail component here */}
-      {/* You can also import from the @features folder */}
-    </div>
-  )
+  return <div className="flex flex-col gap-10"></div>
 }
