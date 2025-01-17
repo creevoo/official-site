6679c40cf4cf68cffc9409f17ae3bafc288d8725
Nyoman Sunima <nyomansunima@gmail.com>
2025-01-17 09:36:13 +0800

    🚜 refactor: update the metadata

--- src/shared/libs/shared-metadata.ts
+++ src/shared/libs/shared-metadata.ts
@@ -2,17 +2,21 @@ import { OpenGraph } from 'next/dist/lib/metadata/types/opengraph-types'
 import { Twitter } from 'next/dist/lib/metadata/types/twitter-types'
 
 const openGraph: OpenGraph = {
-  images: ['YOUR_IMAGE_META_TAG'],
+  images: [
+    'https://cdn.hashnode.com/res/hashnode/image/upload/v1737017115720/bf93d99c-42e7-4d2c-b1e6-6fac1629b111.png',
+  ],
   locale: 'en_US',
   type: 'website',
-  siteName: 'YOUR_WEBSITE_NAME',
+  siteName: 'Download Stunning Website Templates - Creevoo',
 }
 
 const twitter: Twitter = {
-  images: ['YOUR_IMAGE_META_TAG'],
+  images: [
+    'https://cdn.hashnode.com/res/hashnode/image/upload/v1737017115720/bf93d99c-42e7-4d2c-b1e6-6fac1629b111.png',
+  ],
   card: 'summary_large_image',
-  creator: '@YOUR_TWITTER_HANDLE',
-  site: 'YOUR_WEBSITE_NAME',
+  creator: '@nyomansunima',
+  site: 'Download Stunning Website Templates - Creevoo',
 }
 
 export const sharedMetadata = {
