d0d7094c4ac60dd454f3536bcd7350c4f381108b
Nyoman Sunima <nyomansunima@gmail.com>
2025-01-17 09:46:09 +0800

    🚕 feat: add product detail page

---
+++ src/app/products/[slug]/page.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react'
+import { Metadata } from 'next'
+import { sharedMetadata } from '@shared/libs'
+import {
+  ProductContent,
+  ProductImage,
+  ProductInfo,
+  ProductSummary,
+  ProductSupport,
+} from '@features/products'
+
+export const metadata: Metadata = {
+  title: 'Products - Creevoo',
+  description:
+    'Explore stunning website templates with multi platform supported',
+  openGraph: {
+    ...sharedMetadata.openGraph,
+    title: 'Products - Creevoo',
+    description:
+      'Explore stunning website templates with multi platform supported',
+  },
+  twitter: {
+    ...sharedMetadata.twitter,
+    title: 'Products - Creevoo',
+    description:
+      'Explore stunning website templates with multi platform supported',
+  },
+}
+
+export default function ProductDetailPage(): React.ReactElement {
+  return (
+    <div className="flex flex-col gap-10">
+      <ProductInfo />
+      <ProductImage />
+      <ProductSummary />
+      <ProductContent />
+      <ProductSupport />
+    </div>
+  )
+}
