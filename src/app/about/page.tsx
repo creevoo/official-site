db4d4f3905ea5adaeec89565a7a9efad6b0d200a
Nyoman Sunima <nyomansunima@gmail.com>
2025-01-18 21:43:17 +0800

    🚕 feat: add about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,28 +1,29 @@
 import * as React from 'react'
 import { Metadata } from 'next'
 import { sharedMetadata } from '@shared/libs'
-
-import { ArticleContent } from '@shared/components'
+import { AboutSection, MissionSection, TeamSection } from '@features/about'
 
 export const metadata: Metadata = {
-  title: 'About',
+  title: 'About | Creevoo',
   description: 'The team behind creevo and story of our journeys',
   openGraph: {
     ...sharedMetadata.openGraph,
-    title: 'About',
+    title: 'About | Creevoo',
     description: 'The team behind creevo and story of our journeys',
   },
   twitter: {
     ...sharedMetadata.twitter,
-    title: 'About',
+    title: 'About | Creevoo',
     description: 'The team behind creevo and story of our journeys',
   },
 }
 
 export default function AboutPage(): React.ReactElement {
   return (
-    <div className="flex flex-col">
-      <ArticleContent>Hello from about</ArticleContent>
+    <div className="flex flex-col gap-20">
+      <AboutSection />
+      <MissionSection />
+      <TeamSection />
     </div>
   )
 }
