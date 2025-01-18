7c654ea9329f52e90e6d1dc3c3fd8f38a3207542
Nyoman Sunima <nyomansunima@gmail.com>
2025-01-18 21:43:34 +0800

    🚕 feat: add home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,25 +1,42 @@
 import * as React from 'react'
 import { Metadata } from 'next'
 import { sharedMetadata } from '@shared/libs'
+import {
+  FAQSection,
+  FeatureSection,
+  FreeSection,
+  HeroSection,
+  PlatformSection,
+  TrendSection,
+} from '@features/home'
 
 export const metadata: Metadata = {
-  title: 'Stunning Website Templates - Creevoo',
+  title: 'Stunning Website Templates | Creevoo',
   description:
     'Get free and use stunning website template for your landing page, blog, portfolio and business',
   openGraph: {
     ...sharedMetadata.openGraph,
-    title: 'Stunning Website Templates - Creevoo',
+    title: 'Stunning Website Templates | Creevoo',
     description:
       'Get free and use stunning website template for your landing page, blog, portfolio and business',
   },
   twitter: {
     ...sharedMetadata.twitter,
-    title: 'Stunning Website Templates - Creevoo',
+    title: 'Stunning Website Templates | Creevoo',
     description:
       'Get free and use stunning website template for your landing page, blog, portfolio and business',
   },
 }
 
 export default function HomePage(): React.ReactElement {
-  return <div className="flex flex-col gap-10"></div>
+  return (
+    <div className="flex flex-col gap-20">
+      <HeroSection />
+      <FeatureSection />
+      <FreeSection />
+      <PlatformSection />
+      <TrendSection />
+      <FAQSection />
+    </div>
+  )
 }
