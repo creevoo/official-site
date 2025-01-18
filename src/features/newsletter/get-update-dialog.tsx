37670073f4b339e1339d8078bde1129ce4f4a619
Nyoman Sunima <nyomansunima@gmail.com>
2025-01-18 21:53:28 +0800

    🚕 feat(ui): add update dialog

---
+++ src/features/newsletter/get-update-dialog.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react'
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@shared/components'
+import { SubscribeForm } from './subscribe-form'
+
+export function GetUpdateDialog(): React.ReactElement {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button
+          size={'base'}
+          className="transition-all duration-300 hover:-translate-y-1"
+        >
+          <i className="fi fi-br-envelope-dot" />
+          Get Updates
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-md">
+        <DialogHeader>
+          <DialogTitle>Get weekly updates</DialogTitle>
+          <DialogDescription>
+            Get notified when we release new products, features, and updates.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="flex flex-col gap-3 mt-10 pl-2">
+          <SubscribeForm />
+        </div>
+      </DialogContent>
+    </Dialog>
+  )
+}
