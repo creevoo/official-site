ff626b0c316d2a3d5cefc07db1c54c505f6e9c99
Nyoman Sunima <nyomansunima@gmail.com>
2025-01-18 21:53:01 +0800

    🚕 feat(logic): add newsletter service

---
+++ src/features/newsletter/newsletter-service.ts
@@ -0,0 +1,21 @@
+'use server'
+
+import { loopsAPI } from '@shared/libs/connections'
+
+export async function subscribe(values: { email: string }): Promise<void> {
+  const endpoint = '/v1/contacts/create'
+
+  const parsedName = values.email.split('@')[0]
+  const payload = {
+    email: values.email,
+    firstName: parsedName,
+    mailingLists: {
+      cm624i1n500pk0mjphqxo8ype: true,
+    },
+  }
+
+  await loopsAPI(endpoint, {
+    method: 'POST',
+    body: JSON.stringify(payload),
+  })
+}
