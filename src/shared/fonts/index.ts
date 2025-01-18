30ff7324af026cb1ce2950ee8c5c6c6574e75bfa
Nyoman Sunima <nyomansunima@gmail.com>
2025-01-18 21:49:43 +0800

    🚕 feat(font): add jetbrain mono font

--- src/shared/fonts/index.ts
+++ src/shared/fonts/index.ts
@@ -1,13 +1,18 @@
-import { Inter } from 'next/font/google'
+import { Inter, JetBrains_Mono } from 'next/font/google'
 
-// TODO: Add your font here
-// and add to tailwind.config.js
 const inter = Inter({
   display: 'swap',
   subsets: ['latin'],
   variable: '--font-inter',
 })
 
+const jetBrainsMono = JetBrains_Mono({
+  display: 'swap',
+  subsets: ['latin'],
+  variable: '--font-jetbrains-mono',
+})
+
 export const fonts = {
   inter,
+  jetBrainsMono,
 }
