877a547563aa4436fafa1c5e5c075ebde395b44b
Nyoman Sunima <nyomansunima@gmail.com>
2025-01-20 18:30:55 +0800

    🚜 refactor: change the content using dynamic product content

--- src/features/products/product-content.tsx
+++ src/features/products/product-content.tsx
@@ -1,25 +1,16 @@
 import { ArticleContent } from '@shared/components'
 import * as React from 'react'
+import { ProductDetail } from './product-service'
 
-export function ProductContent(): React.ReactElement {
+type Props = {
+  product: ProductDetail
+}
+
+export function ProductContent({ product }: Props): React.ReactElement {
+  const { content } = product
   return (
     <div className="flex">
-      <ArticleContent>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora,
-          delectus ratione adipisci fugit nam quaerat dicta consectetur.
-          Excepturi consectetur temporibus vero eos cum, atque dolore debitis
-          aspernatur totam eligendi facilis ab voluptatibus incidunt odio
-          suscipit ipsa distinctio sunt obcaecati magni.
-        </p>
-
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
-          explicabo quibusdam laboriosam, debitis corrupti in? Reprehenderit
-          sequi cumque eaque architecto, similique praesentium quia quos! Ea
-          quasi fugit dolores possimus consequatur.
-        </p>
-      </ArticleContent>
+      <ArticleContent>{content}</ArticleContent>
     </div>
   )
 }
