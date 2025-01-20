a557d1128573d526ec960de067c7af08fee5c0e8
Nyoman Sunima <nyomansunima@gmail.com>
2025-01-20 16:58:58 +0800

    🚜 refactor: change the hero section

--- src/features/home/hero-section.tsx
+++ src/features/home/hero-section.tsx
@@ -1,5 +1,34 @@
+import { Button, SectionLabel } from '@shared/components'
+import Link from 'next/link'
 import * as React from 'react'
 
 export function HeroSection(): React.ReactElement {
-  return <section></section>
+  return (
+    <section className="flex flex-col">
+      <div className="flex items-center gap-2">
+        <SectionLabel>10+ Free templates</SectionLabel>
+      </div>
+      <h2 className="text-xl font-medium text-balance mt-4">
+        The perfect website templates
+        <br />
+        for every needs
+      </h2>
+      <p className="text-foreground/60 !leading-relaxed mt-3 text-pretty tablet:text-balance">
+        Beautifully crafted templates for your landing page, blog, portfolio and
+        business. Built into different platforms. Easy to use and customize.
+      </p>
+
+      <div className="flex items-center mt-10">
+        <Button
+          className="transition-all duration-300 hover:-translate-y-1"
+          asChild
+        >
+          <Link href={'/products'}>
+            Explore templates
+            <i className="fi fi-rr-angle-small-right" />
+          </Link>
+        </Button>
+      </div>
+    </section>
+  )
 }
