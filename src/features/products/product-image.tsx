3779a98d22c8d075c8cbb2395537c8d494a836f8
Nyoman Sunima <nyomansunima@gmail.com>
2025-01-20 18:30:29 +0800

    🚜 refactor: add props, images and selection image

--- src/features/products/product-image.tsx
+++ src/features/products/product-image.tsx
@@ -1,37 +1,49 @@
+'use client'
+
 import Image from 'next/image'
 import * as React from 'react'
+import { ProductDetail } from './product-service'
 
-export function ProductImage(): React.ReactElement {
+type Props = {
+  product: ProductDetail
+}
+
+export function ProductImage({ product }: Props): React.ReactElement {
+  const { images, title } = product
+  const [selectedImage, setSelectedImage] = React.useState<string>(images[0])
+
+  function onOpenPreview(img: string) {
+    setSelectedImage(img)
+  }
+
   return (
-    <div className="flex gap-3">
+    <div className="flex flex-col tablet:flex-row gap-3">
       <div className="flex p-1 border border-border rounded-2xl flex-1">
-        <picture className="flex relative overflow-hidden rounded-xl h-[400px] w-full">
+        <picture className="flex relative overflow-hidden rounded-xl h-[240px] tablet:h-[400px] w-full">
           <Image
-            src={
-              'https://cdn.dribbble.com/userupload/10136128/file/original-14076d5ccaf47ee446acda3dc4b74d5d.png?resize=1504x1128&vertical=center'
-            }
-            alt=""
+            src={selectedImage}
+            alt={title}
             fill
-            objectFit="cover"
-            className=""
+            priority
+            className="object-cover"
           />
         </picture>
       </div>
 
-      <div className="flex flex-col items-center gap-1 py-3">
-        {Array.from({ length: 4 }).map((_, i) => (
+      <div className="flex flex-wrap tablet:flex-col items-center gap-1 py-3">
+        {images.map((img, i) => (
           <div
-            className="border-1 border-primary rounded-lg overflow-hidden cursor-pointer"
             key={i}
+            onClick={() => onOpenPreview(img)}
+            className="border-1 border-primary rounded-lg overflow-hidden cursor-pointer"
           >
             <Image
-              src={
-                'https://cdn.dribbble.com/userupload/10136128/file/original-14076d5ccaf47ee446acda3dc4b74d5d.png?resize=1504x1128&vertical=center'
-              }
-              alt=""
+              src={img}
+              alt={title}
               width={70}
               height={70}
-              objectFit="cover"
+              priority={false}
+              className="object-cover w-auto h-auto"
             />
           </div>
         ))}
