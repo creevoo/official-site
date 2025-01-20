b40d00dd88f8a70d96ec00fe4a6fbbdd61e3d390
Nyoman Sunima <nyomansunima@gmail.com>
2025-01-20 16:55:33 +0800

    🚜 refactor: change the description and add link

--- src/features/products/product-support.tsx
+++ src/features/products/product-support.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@shared/components'
+import Link from 'next/link'
 import * as React from 'react'
 
 export function ProductSupport(): React.ReactElement {
@@ -6,17 +7,17 @@ export function ProductSupport(): React.ReactElement {
     <div className="flex flex-col p-3 rounded-2xl bg-primary text-primary-foreground mt-16">
       <h2 className="text-lg">Need help?</h2>
 
-      <p className="text-primary-foreground/60 mt-2 text-sm">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores porro
-        itaque autem ab voluptatum quae maxime officiis velit veritatis quod
-        asperiores repellat mollitia illo impedit, in modi, provident est
-        consequatur.
+      <p className="text-primary-foreground/60 mt-2 text-sm text-balance">
+        We offer support for all our templates. You can use our team service to
+        help you start with the template.
       </p>
 
       <div className="flex mt-5">
-        <Button variant={'outline'} size={'sm'}>
-          <i className="fi fi-rr-headset" />
-          Contact us
+        <Button variant={'outline'} size={'sm'} asChild>
+          <Link href={'https://www.tryweebo.one'} target="_blank">
+            <i className="fi fi-rr-headset" />
+            Use Weebo
+          </Link>
         </Button>
       </div>
     </div>
