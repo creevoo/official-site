5bfaa1da4fe78b42a86a3f7383c17ab3446627e8
Nyoman Sunima <nyomansunima@gmail.com>
2025-05-28 16:31:50 +0800

    feat: create success dialog

---
+++ src/features/newsletter/success-join-waitlist-dialog.tsx
@@ -0,0 +1,37 @@
+import {
+  ArticleContent,
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@shared/components"
+
+type Props = {
+  open: boolean
+  onOpenChange: (state: boolean) => void
+}
+
+export function SuccessJoinWaitlistDialog({ open, onOpenChange }: Props) {
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>🎉 Hurray, you're awesome</DialogTitle>
+        </DialogHeader>
+
+        <div className="flex flex-col mt-6">
+          <ArticleContent>
+            <p>
+              Thank you so much to join the waiting list. We'll send you
+              something when creevoo is launching.
+            </p>
+
+            <span className="font-mono text-accent tracking-tight text-sm">
+              Creevoo, awesome template
+            </span>
+          </ArticleContent>
+        </div>
+      </DialogContent>
+    </Dialog>
+  )
+}
