35354d53949b48e0bc43f4c3310f52edf69d1ac6
Nyoman Sunima <nyomansunima@gmail.com>
2025-05-28 16:30:49 +0800

    refactor: update component imports

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,14 +1,10 @@
 export * from "./ui/accordion"
 export * from "./ui/button"
-export * from "./ui/tooltip"
 export * from "./ui/input"
 export * from "./ui/dialog"
 export * from "./ui/form"
-export * from "./ui/toast"
 
-export * from "./common/content"
-export * from "./common/footer"
-export * from "./common/header"
-export * from "./common/section-label"
-
-export * from "./animation/transition"
+export * from "./content"
+export * from "./footer"
+export * from "./header"
+export * from "./section-label"
